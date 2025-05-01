978ac468c98e316da667fcbb6eda88a96584e6c3
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2025-05-02 00:01:49 +0200

    created Admin Contoller  Model Route

---
+++ model/adminAppointmentModel.js
@@ -0,0 +1,27 @@
+// adminAppointmentModel.js
+const db = require("../config/db"); // Assurez-vous d'importer votre connexion DB
+
+// Récupérer tous les rendez-vous
+const getAllAppointments = () => {
+	return new Promise((resolve, reject) => {
+		db.query("SELECT * FROM appointments", (err, results) => {
+			if (err) return reject(err);
+			resolve(results);
+		});
+	});
+};
+
+// Supprimer un rendez-vous
+const deleteAppointmentById = (id) => {
+	return new Promise((resolve, reject) => {
+		db.query("DELETE FROM appointments WHERE id = ?", [id], (err, results) => {
+			if (err) return reject(err);
+			resolve(results);
+		});
+	});
+};
+
+module.exports = {
+	getAllAppointments,
+	deleteAppointmentById,
+};
